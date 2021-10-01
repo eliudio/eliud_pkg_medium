3a9d79adc269c955a3bc6459f88a05d3cda9cfea
Eliud <58745656+eliudio@users.noreply.github.com>
2021-10-01 14:53:47 +0100

    Initial version of conditions

--- lib/medium_package.dart
+++ lib/medium_package.dart
@@ -7,6 +7,8 @@ import 'package:flutter_bloc/src/bloc_provider.dart';
 import 'package:eliud_core/model/access_model.dart';
 
 abstract class MediumPackage extends Package {
+  MediumPackage() : super('eliud_pkg_medium');
+
   @override
   BlocProvider? createMainBloc(NavigatorBloc navigatorBloc, AccessBloc accessBloc) => null;
 
