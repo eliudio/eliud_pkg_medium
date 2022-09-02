1a3acb863086cb037d9c6502aa3c6d98119e7bfb
Eliud <58745656+eliudio@users.noreply.github.com>
2022-09-02 16:45:58 +0100

    Bugfix

--- lib/tools/media_buttons.dart
+++ lib/tools/media_buttons.dart
@@ -13,11 +13,13 @@ import 'package:tuple/tuple.dart';
 typedef Tuple2<MemberMediumAccessibleByGroup,
     List<String>?> MemberMediumAccessibleProviderFunction();
 
+typedef PrivilegeLevelRequiredSimple PlatformMediumAccessibleProviderFunction();
+
 class MediaButtons {
+  // memberMediaButtons
   static PopupMenuButton mediaButtons(
     BuildContext context,
     AppModel app,
-    String ownerId,
     MemberMediumAccessibleProviderFunction accessibleByFunction, {
     MediumAvailable? photoFeedbackFunction,
     FeedbackProgress? photoFeedbackProgress,
@@ -95,4 +97,81 @@ class MediaButtons {
           }
         });
   }
-}
+
+  // platformMediaButtons
+  static PopupMenuButton platformMediaButtons(
+      BuildContext context,
+      AppModel app,
+      PlatformMediumAccessibleProviderFunction accessibleByFunction, {
+        MediumAvailable? photoFeedbackFunction,
+        FeedbackProgress? photoFeedbackProgress,
+        MediumAvailable? videoFeedbackFunction,
+        FeedbackProgress? videoFeedbackProgress,
+        Widget? icon,
+        bool? allowCrop,
+        String? tooltip,
+      }) {
+    var items = <PopupMenuItem<int>>[];
+    if (photoFeedbackFunction != null) {
+      if (Registry.registry()!.getMediumApi().hasCamera()) {
+        items.add(
+          popupMenuItem<int>(app, context, label: 'Take photo', value: 0),
+        );
+      }
+      items.add(
+          popupMenuItem<int>(app, context, label: 'Upload photo', value: 1));
+    }
+    if (videoFeedbackFunction != null) {
+      if (Registry.registry()!.getMediumApi().hasCamera()) {
+        items.add(
+          popupMenuItem<int>(app, context, label: 'Take video', value: 2),
+        );
+      }
+      items.add(new PopupMenuItem<int>(
+          child: text(app, context, 'Upload video'), value: 3));
+    }
+    return popupMenuButton(app, context,
+        tooltip: tooltip,
+        child: icon,
+        itemBuilder: (_) => items,
+        onSelected: (choice) {
+          if (photoFeedbackFunction != null) {
+            if (choice == 0) {
+              Registry.registry()!.getMediumApi().takePhoto(
+                  context,
+                  app,
+                      () => PlatformMediumAccessRights(accessibleByFunction()),
+                  photoFeedbackFunction,
+                  photoFeedbackProgress,
+                  allowCrop: allowCrop);
+            }
+            if (choice == 1) {
+              Registry.registry()!.getMediumApi().uploadPhoto(
+                  context,
+                  app,
+                      () => PlatformMediumAccessRights(accessibleByFunction()),
+                  photoFeedbackFunction,
+                  photoFeedbackProgress,
+                  allowCrop: allowCrop);
+            }
+          }
+          if (videoFeedbackFunction != null) {
+            if (choice == 2) {
+              Registry.registry()!.getMediumApi().takeVideo(
+                  context,
+                  app,
+                      () => PlatformMediumAccessRights(accessibleByFunction()),
+                  videoFeedbackFunction,
+                  videoFeedbackProgress);
+            }
+            if (choice == 3) {
+              Registry.registry()!.getMediumApi().uploadVideo(
+                  context,
+                  app,
+                      () => PlatformMediumAccessRights(accessibleByFunction()),
+                  videoFeedbackFunction,
+                  videoFeedbackProgress);
+            }
+          }
+        });
+  }}
